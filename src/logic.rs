dedda62fbc2885c5578f1e702a9900b4a14ed6de
theocheng <zlz_gty@foxmail.com>
2025-05-18 09:46:48 +0800

    refactor(logic): 重构逻辑处理并优化 UI 与 Backend 的交互方式
    - 简化了 Backend 中的 on_add 回调，直接使用闭包进行逻辑处理
    - 更新了 app.slint 文件，将 add 回调改为带有参数和返回值的函数
    - 调整了 UI 中的加法操作，先调用 Backend 的 add 方法再更新 number 属性

--- src/logic.rs
+++ src/logic.rs
@@ -5,16 +5,7 @@ pub fn impl_logic_for_backend(ui_weak: Weak<AppWindow>) {
     if let Some(strong_ui) = ui_weak.clone().upgrade() {
         let backend = strong_ui.global::<Backend>();
 
-        backend.on_add({
-            let ui_weak = ui_weak.clone();
-            move || {
-                if let Some(strong_ui) = ui_weak.upgrade() {
-                    let backend = strong_ui.global::<Backend>();
-
-                    backend.set_number(backend.get_number() + 1);
-                }
-            }
-        });
+        backend.on_add(|num| num + 1);
 
         backend.on_sub({
             let ui_weak = ui_weak.clone();
