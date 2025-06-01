38c2a68711c8496ddaa1a3159ae1411f1124464c
xcrong <hi@xcrong.me>
2025-06-01 15:19:28 +0800

    feat: 添加配置文件加载异常处理
    - 增加了对配置文件不存在的情况的处理
    - 使用 native-dialog 创建确认对话框，提示用户配置文件已创建
    - 优化了配置文件读取逻辑，提高了代码的健壮性

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,26 @@
 use crate::{AppWindow, Backend};
+use native_dialog::{DialogBuilder, MessageLevel};
 use slint::{ComponentHandle, Weak};
 
 pub fn init(ui_weak: Weak<AppWindow>) {
     // 启动前事件
     // 加载配置文件
-    let number = std::fs::read_to_string("config.txt")
-        .unwrap()
-        .trim()
-        .parse::<i32>()
-        .unwrap();
+    let config_file = match std::fs::read_to_string("config.txt") {
+        Ok(config) => config,
+        Err(_) => {
+            let _ = DialogBuilder::message()
+                .set_title("警告")
+                .set_text("配置文件不存在，已创建默认配置文件")
+                .set_level(MessageLevel::Info)
+                .confirm()
+                .show();
+
+            std::fs::write("config.txt", "0").unwrap();
+            "0".to_string()
+        }
+    };
+
+    let number = config_file.trim().parse::<i32>().unwrap();
     let ui_weak = ui_weak.clone();
     if let Some(ui_strong) = ui_weak.upgrade() {
         let backend = ui_strong.global::<Backend>();
